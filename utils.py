9aba615d6ffc04670b68555d35d7f28f3e1032ee
sinbc2003 <128018744+sinbc2003@users.noreply.github.com>
2024-09-05 23:39:32 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,39 +1,38 @@
 from sentence_transformers import SentenceTransformer
 import pinecone
-import openai
+from openai import OpenAI
 import streamlit as st
 
-openai.api_key = "" ## find at platform.openai.com
+client = OpenAI(api_key="")  # Replace with your OpenAI API key
+
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-pinecone.init(api_key='', # find at app.pinecone.io
-              environment='' # next to api key in console
-             )
-index = pinecone.Index('' # index name from pinecone)
-
+pinecone.init(
+    api_key='',  # Replace with your Pinecone API key
+    environment=''  # Replace with your Pinecone environment
+)
+index = pinecone.Index('')  # Replace with your Pinecone index name
 
 def find_match(input):
     input_em = model.encode(input).tolist()
-    result = index.query(input_em, top_k=2, includeMetadata=True)
-    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+    result = index.query(input_em, top_k=2, include_metadata=True)
+    return result['matches'][0]['metadata']['text'] + "\n" + result['matches'][1]['metadata']['text']
 
 def query_refiner(conversation, query):
-
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+    response = client.completions.create(
+        model="text-davinci-003",
+        prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
-    return response['choices'][0]['text']
+    return response.choices[0].text
 
 def get_conversation_string():
     conversation_string = ""
     for i in range(len(st.session_state['responses'])-1):
-        
-        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
-        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+        conversation_string += f"Human: {st.session_state['requests'][i]}\n"
+        conversation_string += f"Bot: {st.session_state['responses'][i+1]}\n"
     return conversation_string
